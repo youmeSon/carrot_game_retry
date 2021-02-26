b84b4d34edb51c86e12c3e68bdc1840405c09c6d
Youme <winellyoume@gmail.com>
2021-02-26 14:54:42 +0000

    JS: Run 'start' button

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-const carrot_size = 80;
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+  if(started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+}
+function stopGame() {
+
+}
+
+function showStopButton() {
+  const icon = document.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible';
+  gameScore.style.visibility = 'visible';
+}
 
 function initGame() {
+  field.innerHTML = '';
+  gameScore.innerText = CARROT_COUNT;
   console.log(fieldRect);
-  addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/bug.png');
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+  addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
   const x1 = 0;
   const y1 = 0;
-  const x2 = fieldRect.width -carrot_size;
-  const y2 = fieldRect.height - carrot_size;
+  const x2 = fieldRect.width -CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
   for(let i = 0; i < count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
@@ -30,4 +71,4 @@ function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-initGame();
+
