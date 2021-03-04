cd572923b28eac3ea8e204ce4407db3356ab894f
Youme <winellyoume@gmail.com>
2021-03-04 11:31:46 +0000

    JS: Add sound effect

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ const popUp = document.querySelector('.pop-up');
 const popUpMessage = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -36,17 +42,27 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
   showPopUpWithText('Replay❓');
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
+  if(win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
   showPopUpWithText(win? 'YOU WON 🎉' : 'YOU LOST 💩');
 }
 
@@ -98,6 +114,7 @@ function hidePopUp() {
 }
 
 function initGame() {
+  score = 0;
   field.innerHTML = '';
   gameScore.innerText = CARROT_COUNT;
   console.log(fieldRect);
@@ -113,15 +130,23 @@ function onFieldClick(event) {
   if(target.matches('.carrot')) {
     target.remove();
     score++;
+    playSound(carrotSound);
     updateScoreBoard();
     if(score == CARROT_COUNT) {
-      stopGameTimer();
       finishGame(true);
     }
   } else if (target.matches('.bug')) {
-    stopGameTimer();
     finishGame(false);
   }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
 }
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
